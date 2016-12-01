2f743966e7731cf57b0fb01130b527b30e704710
Steffen Köhler <steffen.koehler@sovanta.com>
2016-12-01 12:28:00 +0100

    init commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+var config = {
+  center: [48.137154, 	11.576124],
+  bounds: L.latLngBounds([48.000, 11.245], [48.634, 11.823]),
+  minSalary: 0,
+  maxSalary: 5000,
+  startSalary: 1650,
+  startRoom: 2,
+  colors: ['#d9ef8b', '#ffeaad', '#fdae61', '#d73027', '#a50026'],
+  roomQms: {
+    1: 40,
+    2: 72,
+    3: 90,
+    4: 120
+  }
+};
