8a03ee9b7fa9138f83a9fb28e852a953b1f98e93
Steffen Köhler <steffen.koehler@sovanta.com>
2016-12-19 13:44:26 +0100

    [fix] avoid jumping from makers (wohnheime) when zooming the map

--- js/main.js
+++ js/main.js
@@ -473,7 +473,7 @@ var rentApp = (function (window, document, $, L, undefined) {
             iconUrl: 'img/wohnheime.png',
 
             iconSize: [50, 50], // size of the icon
-            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            iconAnchor: [25,50], // point of the icon which will correspond to marker's location
             popupAnchor: [2, -90] // point from which the popup should open relative to the iconAnchor
         });
 
